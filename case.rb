3475cad82b0a6e79b0d687940e8a049fbe8441ce
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-12 16:06:12 -0300

    commit case.rb

---
+++ case.rb
@@ -0,0 +1,16 @@
+puts 'digite o numero do mes em que voce nasceu'
+
+  month = gets.chomp.to_i
+
+  case month
+  when 1..3
+    puts 'voce nasceu no começo do ano'
+  when 9...12
+    puts 'voce nasceu no final do ano'
+  when 4...6
+    puts 'voce nasceu na primeira metade do ano'
+  when 7...8
+    puts 'voce nasceu na segunda metade do ano'
+  else
+    puts 'nao foi possível identificar'
+  end
