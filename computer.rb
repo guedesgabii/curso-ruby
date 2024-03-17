723159a0ba41aa9cd1de2e8748f27d57d653c57a
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-16 21:34:57 -0300

    commit - computer

---
+++ computer.rb
@@ -0,0 +1,13 @@
+class Computer
+  def ligar
+          'ligar o computador'
+  end
+
+  def desligar
+          'desligar o computador'
+  end
+end
+
+computer = Computer.new
+puts computer.ligar
+puts computer.desligar
