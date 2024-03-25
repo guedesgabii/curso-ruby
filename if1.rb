7b50e9f0376d78c291c45f29193ba2abbb4d0e6f
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-25 20:46:22 -0300

    commit if1

---
+++ if1.rb
@@ -0,0 +1,19 @@
+x = 10
+
+if x == 11
+    puts "x é igual a 10"
+elsif  x == 50
+  puts " x é igual a 50"
+else
+  puts " x não é igual a 10"
+end
+
+nome = 'Diego'
+
+if nome == "Diego"
+puts "Que nome lindo!"
+elsif nome == "João"
+  puts "nome normal"
+else
+  puts "que nome estranho!"
+end
