5ca3a74b56c1d1cab086019030e97ee050c0d82d
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-25 20:30:51 -0300

    commit numeros1

---
+++ numeros1.rb
@@ -0,0 +1,17 @@
+puts 'insira um numero'
+x = gets.chomp.to_i
+puts 'insira outro numero'
+y = gets.chomp.to_i
+
+
+soma = x + y
+subt = x - y
+mult = x * y
+div = x.to_f/ y.to_f
+
+puts soma
+puts subt
+puts mult
+puts div
+
+puts 'diego' * 5
