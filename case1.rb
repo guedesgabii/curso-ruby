9908b9cad0e06603cf463852b7dc5c37cdede082
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-25 21:17:28 -0300

    commit case

---
+++ case1.rb
@@ -0,0 +1,12 @@
+mes = 'abril'
+
+case mes
+when 'janeiro'
+  puts 'tem 31 dias'
+when 'fevereiro'
+  puts 'tem 28 dias'
+when 'março'
+  puts 'tem 30 dias'
+else
+  puts 'nao sei quantos dias esse mes tem'
+end
