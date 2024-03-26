88655643469175997dd26cb0a9dc5b436e992bec
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-26 11:01:51 -0300

    ternario

---
+++ ternario.rb
@@ -0,0 +1,16 @@
+nome = 'diego'
+
+if nome.eql? ('diego')
+    puts 'dono do canal noob code'
+    else
+      puts 'visitante'
+end
+
+condicao ? verdadeiro : falso
+nome.eql?('diego') ? "dono do canal noob code": "visitante"
+resultado = nome.eql?('diego') ? " dono do canal boob code " - resultado" "visitante - resultado'
+    puts resultado
+    
+    soma = n.eql?(5) ? n + 10: n - 1
+    puts soma
+end
