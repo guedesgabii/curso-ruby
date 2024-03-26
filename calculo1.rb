405c02122578f0673b57792f4ca1ab93ae78faa0
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-26 10:46:42 -0300

    commit calculo1

---
+++ calculo1.rb
@@ -0,0 +1,20 @@
+def soma_metodo(x, y)
+  return x + y
+end
+
+def subt(x, y)
+  x - y
+end
+
+def mult(x, y)
+  x * y
+end
+
+def div(x, y)
+  x / y
+end
+
+puts soma_metodo(2, 4)
+puts subt(5, 6)
+puts mult(3, 9)
+puts div(5, 10)
