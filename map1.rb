53717a6ecb0227e9533e83be372c95c81b26fca8
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-26 10:45:19 -0300

    commit map1

---
+++ map1.rb
@@ -0,0 +1,10 @@
+nomes = ['joao', 'maria','josé','mateus']
+
+nomes_completos = nomes.map do [nome nomes_completos]
+    nome_completo + 'sobrenome'
+
+end
+
+puts nomes
+puts "----------"
+puts nomes_completos
