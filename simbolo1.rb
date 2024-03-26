4b06aa45f48ddf1ebe2682e80095c8a029b7c8a1
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-25 21:41:21 -0300

    commit simbolo1

---
+++ simbolo1.rb
@@ -0,0 +1,9 @@
+puts :nome.object_id
+puts 'nome'.object_id
+puts 'nome'.object_id
+puts :nome.object_id
+
+before_action :get_name
+
+hashes = {nome: 'Diego', idade: 35, nome2: 'João', idade2:60}
+puts hashes
