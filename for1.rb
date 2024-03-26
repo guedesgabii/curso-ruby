f2221f33c28d377a24beadfded2da9fb7e5f52dd
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-26 06:02:43 -0300

    commit for1

---
+++ for1.rb
@@ -0,0 +1,11 @@
+nomes = ['joão', 'maria','josé', 'mateus']
+
+dict = {nome: 'diego', idade: 35, altura: 1,73}
+
+for nome in nomes do
+    puts nomes
+end
+
+for k, v in dict do
+    puts '#{k}: #{v}'
+end
