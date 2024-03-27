4903b732412a602bc5dd7ed1134f0c21c19a1476
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-27 16:25:58 -0300

    commit classe aluno2

---
+++ classe/aluno2.rb
@@ -0,0 +1,13 @@
+class Aluno
+    attr_accessor : nome, :idade
+    def initialize (nome, idade)
+            @nome = nome
+            @idade = idade
+    end
+end
+
+a1=aluno.new
+
+
+puts al.nome
+puts a1.idade
