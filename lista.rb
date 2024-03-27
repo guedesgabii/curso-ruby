84fa25cb6dfe898e064113c8285f3d4747684f38
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-27 15:49:58 -0300

    commit lista

---
+++ lista.rb
@@ -0,0 +1,17 @@
+lista =[]
+
+lista. push ('Diego', 'joão')
+lista << 'maria'
+
+
+
+lista.insert {0, "fulano"}
+lista. insert {2. "siclano"}
+lista.delete{'siclano'}
+
+lista_organizada = lista.sort
+puts lista_organizada.last
+
+puts lista. length
+puts lista. sort
+puts lista. first
