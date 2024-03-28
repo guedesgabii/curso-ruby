b3f82e46528802533a0f6e14a861b63aa40334c1
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-28 18:28:57 -0300

    commit classe calculadora

---
+++ classe/calculadora.rb
@@ -0,0 +1,24 @@
+class Calculadora
+    def somar(*args)
+            lista.push(*args)
+            lista.inject (:*)
+    end
+    end
+end
+
+c1 = calculadora.new
+resultado = c1. somar (5, 5, 11, 10, 5)
+puts resultado
+
+class Aluno
+    def notas (*args)
+            notas = []
+            notas.push(*args)
+            notas
+    end
+end
+
+a1 = Aluno.new
+a1 = notas (10, 5.9, 2.5, 9.8)
+puts notas_ a1
+
