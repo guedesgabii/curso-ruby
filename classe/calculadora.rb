cd35149cf1af3cf9d99d9576bf6da3f43fd62298
guedesgabii <guedesgabii.dev@gmail.com>
2024-03-28 19:48:05 -0300

    commit calculadora

--- classe/calculadora.rb
+++ classe/calculadora.rb
@@ -1,14 +1,23 @@
+module Operacoes
+    def subt (x,y)
+            x-y
+    end
+end
+
 class Calculadora
+include Operacoes
+end
     def somar(*args)
             lista.push(*args)
             lista.inject (:*)
-    end
     end
 end
 
 c1 = calculadora.new
 resultado = c1. somar (5, 5, 11, 10, 5)
+res_subt =c1.subt(10.5)
 puts resultado
+puts res_subt
 
 class Aluno
     def notas (*args)
@@ -21,4 +30,4 @@ end
 a1 = Aluno.new
 a1 = notas (10, 5.9, 2.5, 9.8)
 puts notas_ a1
-
+end
